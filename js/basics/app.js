65c422cdb268c56f14e1e62a9b4ca0e7345a0750
Selim <selimdurmus.italia@gmail.com>
2023-03-24 02:45:37 -0600

    added js folder and basics file

---
+++ js/basics/app.js
@@ -0,0 +1,66 @@
+// let randomNum = Math.random();
+
+// if (randomNum < 0.5) {
+//   console.log("greater", `${randomNum}`);
+// }
+
+// const dayOfWeek = prompt("enter a day").toLowerCase;
+
+// if (dayOfWeek === "monday") {
+//   console.log("I HATE IT");
+// } else if (dayOfWeek === "saturday") {
+//   console.log("way better!");
+// } else if (dayOfWeek === "friday") {
+//   //   console.log("meh!");
+//   // } else if (dayOfWeek === "tuesday") {
+//   //   console.log("meh!");
+//   // } else if (dayOfWeek === "thursday") {
+//   //   console.log("meh!");
+//   // } else if (dayOfWeek === "wednesday") {
+//   //   console.log("meh!");
+// } else if (dayOfWeek === "sunday") {
+//   console.log("yay!");
+// } else console.log("meh");
+
+// const age = 100;
+
+// if (age < 5) {
+//   console.log("you are a baby, it's free");
+// } else if (age < 10) {
+//   console.log("you are a child, it's $10");
+// } else if (age < 65) {
+//   console.log("you are a adult, you pay $20");
+// } else {
+//   console.log("you are a senior, you pay $30");
+// }
+
+// const password = prompt("enter a password");
+
+// if (password.length >= 6) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("Valid password");
+//   } else {
+//     console.log("can't put space in");
+//   }
+// } else {
+//   console.log("not enough characters");
+// }
+
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("valid");
+// } else {
+//   console.log("not valid");
+// }
+
+const age = 1;
+
+switch (age) {
+  case 1:
+    console.log("okay");
+    break;
+  case 2:
+    console.log("not okay");
+    break;
+  default:
+    console.log("what?");
+}
