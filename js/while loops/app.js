f818fbe6aa2692833fb68aee4f80898d5bd4fb79
Selim <selimdurmus.italia@gmail.com>
2023-03-28 09:19:06 -0600

    added the guessins minigame

--- js/while loops/app.js
+++ js/while loops/app.js
@@ -4,11 +4,21 @@
 //   console.log(count);
 // }
 
-const secret = "pass";
+// const secret = "password";
 
-let guess = prompt("guess the secret");
+// let guess = prompt("make a guess");
 
-while (guess !== secret) {
-  guess = prompt("guess the secret");
+// while (guess !== secret) {
+//   guess = prompt("make a guess");
+// }
+
+// console.log("congratz");
+
+let input = prompt("type something");
+
+while (true) {
+  input = prompt(input);
+  if (input === "stop") break;
 }
-console.log("CORRECT!");
+
+console.log("you won");
