ccee4fe521582267a5df766f4bd13b62c931b955
Selim <selimdurmus.italia@gmail.com>
2023-04-04 00:31:29 -0600

    completed functions

--- js/functions/app.js
+++ js/functions/app.js
@@ -33,3 +33,54 @@ function lastElement(x) {
   }
   return x[x.length - 1];
 }
+
+function makeBetweenFunc(min, max) {
+  return function (num) {
+    return num >= min && num <= max;
+  };
+}
+
+function makeBetweenFunc2(min, max) {
+  return function (num) {
+    return num >= min && num <= max;
+  };
+}
+
+const myMath = {
+  PI: 3.14,
+  square(x) {
+    return x * x;
+  },
+  cube(x) {
+    return x ** 3;
+  },
+};
+
+const cat = {
+  name: "Blue",
+  color: "grey",
+  breed: "scottish",
+  meow() {
+    console.log(this.name);
+  },
+};
+
+const hen = {
+  name: "Helen",
+  eggCount: 0,
+  layAnEgg() {
+    this.eggCount += 1;
+    return "EGG";
+  },
+};
+
+function countLetters(word) {
+  let counts = {};
+  for (let i = 0; i < word.length; i++) {
+    let letter = word[i];
+    if (counts[letter]) {
+      counts[letter]++;
+    } else counts[letter] = 1;
+  }
+  return counts;
+}
