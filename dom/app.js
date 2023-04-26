3acfab16b164feb5dee7fd7e00be9fb5e38f2470
Selim <selimdurmus.italia@gmail.com>
2023-04-26 11:16:51 -0600

    added dom folders

---
+++ dom/app.js
@@ -0,0 +1,19 @@
+const allImages = document.getElementsByTagName("img");
+
+function changeImagesToPartridge() {
+  for (let each of allImages) {
+    each.src =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg";
+  }
+}
+
+document.getElementsByClassName("square");
+
+const squareImages = document.getElementsByClassName("square");
+
+function changeSquareImagesToPartridge() {
+  for (let img of squareImages) {
+    img.src =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Partridge_Silkie_hen.jpg/900px-Partridge_Silkie_hen.jpg";
+  }
+}
