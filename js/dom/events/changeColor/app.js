b3194b1189268f495eeefa9cdbabaa4044c9b5a2
Selim <selimdurmus.italia@gmail.com>
2023-05-03 09:00:49 -0600

    changed a few things on changeColor

--- js/dom/events/changeColor/app.js
+++ js/dom/events/changeColor/app.js
@@ -3,12 +3,19 @@ const h1 = document.querySelector("h1");
 const body = document.querySelector("body");
 
 function createColor() {
-  let first = Math.floor(Math.random() * 255 + 1);
-  let second = Math.floor(Math.random() * 255 + 1);
-  let third = Math.floor(Math.random() * 255 + 1);
-  let newColor = `${first},${second},${third}`;
-  h1.innerText = `RGB(${newColor})`;
-  body.bgColor = newColor;
+  let first = Math.floor(Math.random() * 256);
+  let second = Math.floor(Math.random() * 256);
+  let third = Math.floor(Math.random() * 256);
+  let newColor = `RGB(${first},${second},${third})`;
+  h1.innerText = `${newColor}`;
+  document.body.style.backgroundColor = newColor;
+  if (first + second + third <= 250) {
+    h1.style.color = "white";
+    console.log("lesser");
+  } else {
+    h1.style.color = "black";
+    console.log("greater");
+  }
 }
 
 changeButton.addEventListener("click", createColor);
