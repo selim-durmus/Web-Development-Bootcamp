8e4f694cb6801ac28a1e5f76bb04ce995d885534
Selim <selimdurmus.italia@gmail.com>
2023-05-05 10:02:50 -0600

    worked on event form basics

--- js/dom/event object/app.js
+++ js/dom/event object/app.js
@@ -28,3 +28,28 @@ window.addEventListener("keydown", function (e) {
       break;
   }
 });
+
+const form = document.querySelector("#shelter");
+const input2 = document.querySelector("#catName");
+const list = document.querySelector("#cats");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const catName = input2.value;
+  const newLi = document.createElement("li");
+  newLi.innerText = catName;
+  list.append(newLi);
+  input2.value = "";
+});
+
+const tweetForm = document.querySelector("#tweet");
+const tweetInput = document.querySelector("#tweetinput");
+const nameInput = document.querySelector("#nameinput");
+const tweets = document.querySelector("#tweets");
+tweetForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const tweetContent = tweetInput.value;
+  const newLi = document.createElement("li");
+  newLi.innerText = tweetContent;
+  tweets.append(newLi);
+});
