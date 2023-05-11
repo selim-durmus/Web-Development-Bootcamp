db7702f863cc493be3cc87483713b95494c8da32
Selim <selimdurmus.italia@gmail.com>
2023-05-11 10:48:18 -0600

    finished the score keeper

--- js/score keeper/event object/app.js
+++ js/score keeper/event object/app.js
@@ -1,5 +1,5 @@
-const playerOne = document.querySelector("#player_one");
-const playerTwo = document.querySelector("#player_two");
+const playerOneBtn = document.querySelector("#player_one_btn");
+const playerTwoBtn = document.querySelector("#player_two_btn");
 const reset = document.querySelector("#reset");
 const score = document.querySelector("#score_table");
 const playing_to = document.querySelector("#playing_to");
@@ -7,20 +7,56 @@ const playerOneScore = document.querySelector("#player_one_score");
 const playerTwoScore = document.querySelector("#player_two_score");
 let scr1 = 0;
 let scr2 = 0;
-playerOne.addEventListener("click", function () {
-  const playingTo = playing_to.value;
-  scr1 = scr1 + 1;
-  console.log(scr1);
-  playerOneScore.textContent = `${scr1}`;
+playerOneScore.textContent = 0;
+playerTwoScore.textContent = 0;
+let gameEnded = false;
+let playingTo = 5;
+
+playing_to.addEventListener("change", function () {
+  playingTo = parseInt(playing_to.value);
+  resetGame();
 });
 
-playerTwo.addEventListener("click", function () {
-  const playingTo = playing_to.value;
-  scr2 = scr2 + 1;
-  console.log(scr2);
-  playerTwoScore.textContent = `${scr2}`;
+playerOneBtn.addEventListener("click", function () {
+  if (!gameEnded) {
+    const playingTo = parseInt(playing_to.value);
+    scr1 = scr1 + 1;
+    console.log(scr1);
+    playerOneScore.textContent = `${scr1}`;
+  }
+
+  if (scr1 + scr2 === playingTo) {
+    gameEnded = true;
+    playerOneBtn.disabled = true;
+    playerTwoBtn.disabled = true;
+  }
+});
+
+playerTwoBtn.addEventListener("click", function () {
+  if (!gameEnded) {
+    const playingTo = parseInt(playing_to.value);
+    scr2 = scr2 + 1;
+    console.log(scr2);
+    playerTwoScore.textContent = `${scr2}`;
+  }
+
+  if (scr1 + scr2 === playingTo) {
+    gameEnded = true;
+    playerOneBtn.disabled = true;
+    playerTwoBtn.disabled = true;
+  }
 });
 
 reset.addEventListener("click", function () {
-  console.log("clicked on reset");
+  resetGame();
 });
+
+function resetGame() {
+  scr1 = 0;
+  scr2 = 0;
+  playerOneScore.textContent = 0;
+  playerTwoScore.textContent = 0;
+  gameEnded = false;
+  playerOneBtn.disabled = false;
+  playerTwoBtn.disabled = false;
+}
