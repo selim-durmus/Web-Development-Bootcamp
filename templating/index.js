4c040cfd725e4eef23bf28bda9a8bf2ca9f178e5
Selim <selimdurmus.italia@gmail.com>
2024-02-29 22:56:39 -0700

    worked on loops and conditionals in templating

--- templating/index.js
+++ templating/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const redditData = require("./data.json");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -9,9 +10,28 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+app.get("/cats", (req, res) => {
+  const cats = ["Blue", "Rocket", "John", "John", "James"];
+  res.render("cats", { cats });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  const data = redditData[subreddit];
+  if (data) {
+    res.render("subreddit", { ...data });
+  } else {
+    res.render("not_found", { subreddit });
+  }
+});
+function isEven(num) {
+  return num % 2 === 0;
+}
 app.get("/rand", (req, res) => {
   const num = Math.floor(Math.random() * 10) + 1;
-  res.render("random", { num: num });
+  const even = isEven(num);
+
+  res.render("random", { num: num, even: even });
 });
 
 app.listen(3000, () => {
