eccdb6d6ac41c91e271c3cfb53098c1413e68e17
Selim <selimdurmus.italia@gmail.com>
2024-04-06 00:41:34 -0600

    worked on creating RESTful routes

--- RESTful Routing/RESTDemo/index.js
+++ RESTful Routing/RESTDemo/index.js
@@ -1,19 +1,49 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+//app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const comments = [
+  { username: "Todd", comment: "lol that's so funny" },
+  { username: "Skyler", comment: "I like to go birdwatching with my dog" },
+  { username: "Sk8erBoi", comment: "Plz delete your account, Todd" },
+  { username: "onlysayswoof", comment: "woof woof woof" },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
+
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.redirect("/comments");
+});
 
 app.get("/tacos", (req, res) => {
   res.send("GET /tacos response");
 });
 
 app.post("/tacos", (req, res) => {
-  console.log(req.body);
-  const { quantity, meat } = req.body;
-  res.send(`ok here your order: ${quantity} ${meat} tacos`);
+  const { meat, qty } = req.body;
+  res.send(`OK, here are your ${qty} ${meat} tacos`);
 });
 
 app.listen(3000, () => {
   console.log("ON PORT 3000!");
 });
+
+// GET /comments - list all comments
+// POST /comments - Create a new comment
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
